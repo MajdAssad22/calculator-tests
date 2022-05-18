2eead6e13ca31d9c43ffc780ad19c13602f58124
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2022-05-18 18:21:52 +0300

    Basic Calculator with the wanted functions.
    > Not Finished.
    > work on:
    > * Programmer functions.
    > * Support degree and radian

---
+++ CalculatorTestProject/Control/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.Control
+{
+    public class Calculator
+    {
+        public static ObservableCollection<ExpressionTree> History = new ObservableCollection<ExpressionTree>();
+
+        public static double Calculate(string expression)
+        {
+            ExpressionTree tree = new ExpressionTree(expression);
+            History.Add(tree);
+            return tree.Result;
+        }
+    }
+}
---
+++ CalculatorTestProject/Control/CalculatorLogic.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.Control
+{
+    public static class CalculatorLogic
+    {
+        /// <summary>
+        /// This function calculates the given mathematical function on a given number,
+        /// If the function is not defined then the number is returned.
+        /// </summary>
+        /// <param name="function">
+        /// String that indicates which function to execute.
+        /// </param>
+        /// <param name="number">
+        /// Double indicates the number to execute the function on.
+        /// </param>
+        /// <returns>
+        /// Returns the calculated value or the number it self if the function is not defined.
+        /// </returns>
+        public static double CalculateFunction(string function, double number)
+        {
+            switch (function)
+            {
+                case CalculatorParams.SIN_FUNC:
+                    return Math.Sin(number);
+
+                case CalculatorParams.COS_FUNC:
+                    return Math.Cos(number);
+                
+                case CalculatorParams.TAN_FUNC:
+                    return Math.Tan(number);
+                
+                case CalculatorParams.LOG_FUNC:
+                    return Math.Log10(number);
+                
+                case CalculatorParams.LN_FUNC:
+                    return Math.Log(number);
+
+                case CalculatorParams.PERC:
+                    return number / 100;
+
+                case CalculatorParams.ABS_FUNC:
+                    return Math.Abs(number);
+
+                case CalculatorParams.SQRT_FUNC:
+                    return Math.Sqrt(number);
+                
+                default:
+                    return number;
+            }
+        }
+        public static double ConvertToNumber(string data)
+        {
+            if (data.Equals(CalculatorParams.E))
+            {
+                return Math.E;
+            }
+            else if (data.Equals(CalculatorParams.PI))
+            {
+                return Math.PI;
+            }
+            else
+            {
+                return Convert.ToDouble(data);
+            }
+        }
+        public static int GetPriority(string operation)
+        {
+            switch (operation)
+            {
+                case CalculatorParams.POW:
+                    return 3;
+                case CalculatorParams.MULT:
+                case CalculatorParams.DIV:
+                    return 2;
+                case CalculatorParams.ADD:
+                case CalculatorParams.SUB:
+                    return 1;
+                case CalculatorParams.CLOSE_BRACK:
+                    return 0;
+                default:
+                    return -1;
+            }
+        }
+        public static bool IsFunction(string expression)
+        {
+            if (expression.Equals(CalculatorParams.SIN_FUNC)
+                || expression.Equals(CalculatorParams.COS_FUNC)
+                || expression.Equals(CalculatorParams.TAN_FUNC)
+                || expression.Equals(CalculatorParams.LOG_FUNC)
+                || expression.Equals(CalculatorParams.LN_FUNC)
+                || expression.Equals(CalculatorParams.PERC)
+                || expression.Equals(CalculatorParams.ABS_FUNC)
+                || expression.Equals(CalculatorParams.SQRT_FUNC))
+                return true;
+            return false;
+        }
+        public static bool IsMathConst(string expression)
+        {
+            if (expression.Equals(CalculatorParams.E) || expression.Equals(CalculatorParams.PI))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
