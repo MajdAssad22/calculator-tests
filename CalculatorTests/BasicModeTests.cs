be9f2b2870ee4130cd6689da5c9a3592d4b6a31f
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2022-05-22 22:14:03 +0300

    Added the test project

---
+++ CalculatorTests/BasicModeTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorControl;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class BasicModeTests
+    {
+        [TestMethod]
+        public void DivideByZeroTest()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Basic;
+                var result = CalculatorControl.Calculator.Calculate("2 / 0");
+                Assert.AreEqual(Double.PositiveInfinity, result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod]
+        public void AddingBinary()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Programmer;
+                CalculatorControl.Calculator.Base = CalculatorParams.Bases.Bin;
+                var result = Calculator.Calculate("1001 + 11001");
+                Assert.AreEqual(34, result);                   
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod]
+        public void AddingOctal()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Programmer;
+                CalculatorControl.Calculator.Base = CalculatorParams.Bases.Oct;
+                var result = Calculator.Calculate("712 + 761");
+                Assert.AreEqual(955, result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
---
+++ CalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cfc5d0b0-f903-4aa8-a8df-28c20fd84746")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
