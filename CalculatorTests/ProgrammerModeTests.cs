97721b1dedb08aebaf0cba8113128ad1ff26d99e
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2022-05-29 19:53:10 +0300

    Added basic tests and orgenized test files

---
+++ CalculatorTests/ProgrammerModeTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CalculatorControl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class ProgrammerModeTests
+    {
+        [TestMethod]
+        public void AddingBinary()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Programmer;
+                CalculatorControl.Calculator.Base = CalculatorParams.Bases.Bin;
+                var result = Calculator.Calculate("1001 + 11001");
+                Assert.AreEqual("100010", result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod]
+        public void AddingOctal()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Programmer;
+                CalculatorControl.Calculator.Base = CalculatorParams.Bases.Oct;
+                var result = Calculator.Calculate("712 + 761");
+                Assert.AreEqual("1673", result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
---
+++ CalculatorTests/ScientificModeTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Remoting;
+using CalculatorControl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class ScientificModeTests
+    {
+        [TestMethod]
+        public void SinFunctionTest()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Scientific;
+                var result = Calculator.Calculate("sin( 5 + 9 - 1 )");
+                Assert.AreEqual(Math.Sin(13).ToString(), result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod]
+        public void CosFunctionTest()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Scientific;
+                var result = Calculator.Calculate("cos( 5 + 9 - 1 )");
+                Assert.AreEqual(Math.Cos(13).ToString(), result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
