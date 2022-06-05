2e5a1df65ffcfa9e7c9030a3d13806000d0a61b4
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2022-06-05 17:21:15 +0300

    Added some tests for a basic testing procedure

--- CalculatorTests/BasicModeTests.cs
+++ CalculatorTests/BasicModeTests.cs
@@ -21,5 +21,78 @@ namespace CalculatorTests
                 Assert.Fail(e.Message);
             }
         }
+        
+        [TestMethod]
+        public void PercentageTest()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Basic;
+                var result = CalculatorControl.Calculator.Calculate("5 %");
+                Assert.AreEqual("0.05", result);
+                result = CalculatorControl.Calculator.Calculate("% 5");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result); 
+                result = CalculatorControl.Calculator.Calculate("3 * 9 + 2 %");
+                Assert.AreEqual("27.02", result);
+                result = CalculatorControl.Calculator.Calculate("( 10 % )");
+                Assert.AreEqual("0.1", result);
+                result = CalculatorControl.Calculator.Calculate("( 10 + 6 ) %");
+                Assert.AreEqual("0.16", result);
+                result = CalculatorControl.Calculator.Calculate("( 10 + 6 ) % %");
+                Assert.AreEqual("0.0016", result);
+                result = CalculatorControl.Calculator.Calculate("( e ) %");
+                Assert.AreEqual((Math.E / 100).ToString(), result);
+                result = CalculatorControl.Calculator.Calculate("( % 5 + 6 )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("5 + % 5");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("5 + -%");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void OpenBracketTest()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Basic;
+                Calculator.IsDegree = true;
+                var result = CalculatorControl.Calculator.Calculate("5 ( 5 + 6 )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("6 + ( 5 + 6 )");
+                Assert.AreEqual("17", result);
+                result = CalculatorControl.Calculator.Calculate("( + 6 )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("( 8 + )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("( 5");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("sin( ( 4 + 5 ) * 7 )");
+                Assert.AreEqual(Math.Sin(63 * Math.PI / 180).ToString(), result);
+                result = CalculatorControl.Calculator.Calculate("( ( 4 + 5 ) * 7 )");
+                Assert.AreEqual("63", result);
+                result = CalculatorControl.Calculator.Calculate("4 + ( ( 5 ) )");
+                Assert.AreEqual("9", result);
+                result = CalculatorControl.Calculator.Calculate("- ( 5 )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("( 5 + 9 ) ( 5 )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("5 + 9 ( 5 + 8 )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate(" 5 + 9 % ( 5 )");
+                Assert.AreEqual(CalculatorParams.INVALID_INPUT, result);
+                result = CalculatorControl.Calculator.Calculate("100 + -( 3 + 6 )");
+                Assert.AreEqual("91", result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
     }
 }
--- CalculatorTests/ScientificModeTests.cs
+++ CalculatorTests/ScientificModeTests.cs
@@ -14,8 +14,11 @@ namespace CalculatorTests
             try
             {
                 Calculator.Mode = CalculatorParams.CalculatorModes.Scientific;
+                Calculator.IsDegree = false;
                 var result = Calculator.Calculate("sin( 5 + 9 - 1 )");
                 Assert.AreEqual(Math.Sin(13).ToString(), result);
+                result = Calculator.Calculate("sin( -( 5 + 9 ) - 1 )");
+                Assert.AreEqual(Math.Sin(-15).ToString(), result);
             }
             catch (Exception e)
             {
@@ -28,8 +31,24 @@ namespace CalculatorTests
             try
             {
                 Calculator.Mode = CalculatorParams.CalculatorModes.Scientific;
+                Calculator.IsDegree = false;
                 var result = Calculator.Calculate("cos( 5 + 9 - 1 )");
                 Assert.AreEqual(Math.Cos(13).ToString(), result);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod]
+        public void TanFunctionTest()
+        {
+            try
+            {
+                Calculator.Mode = CalculatorParams.CalculatorModes.Scientific;
+                Calculator.IsDegree = false;
+                var result = Calculator.Calculate("tan( π )");
+                Assert.AreEqual(Math.Tan(Math.PI).ToString(), result);
             }
             catch (Exception e)
             {
