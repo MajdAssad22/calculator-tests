13cb82907c56a9057352333fa49627d9960c773e
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2022-08-06 17:54:50 +0300

    Fixed the problem with the difference between the history result and the actual result

--- CalculatorControl/ExpressionTree.cs
+++ CalculatorControl/ExpressionTree.cs
@@ -259,7 +259,7 @@ namespace CalculatorControl
 
         public override string ToString()
         {
-            return $"{Expression} = {Result}";
+            return $"{Expression} = {Calculator.Format(this)}";
         }
 
         private void AddBranch(Stack<string> stC, Stack<Node> stN)
